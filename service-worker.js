87523e429963aab5b96891aa99798952ae698e0b
rikleee <102378874+rikleee@users.noreply.github.com>
2026-07-05 14:00:09 +0200

    chore: refresh cache for expanded CTI manual

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='attack-profiler-pwa-v16-multipage-training';
+const CACHE_NAME='attack-profiler-pwa-v17-expanded-cti-manual';
 const APP_SHELL=['./','./index.html','./intelligence.html','./training.html','./analyze.html','./security.html','./glossary.html','./styles.css','./cti.css','./app.js','./cti.js','./site.js','./global-feed.js','./pages.js','./manifest.json','./manifest.webmanifest','./icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
